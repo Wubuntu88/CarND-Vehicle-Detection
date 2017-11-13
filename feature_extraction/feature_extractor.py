750bf59438a565368e7604b8a8160b59b711a004
William Gillespie <wgilles1@emich.edu>
2017-11-13 18:59:53 -0500

    Added a method that extracts features from a single image and puts them into a feature vector.

--- feature_extraction/feature_extractor.py
+++ feature_extraction/feature_extractor.py
@@ -9,16 +9,22 @@ import feature_extraction.hog_extractor as he
 
 def extract_features(images_array: TypeVar("np.array")) -> list:
     features = []
-    for index in range(0, images_array.shape[0]):
+    for index in range(0, images_array.shape[0]):  # iterates over each image
         rgb_image = images_array[index]  # this is a (64, 64, 3) numpy array
         spatial_features = cs.bin_spatial(img=rgb_image, size=(32, 32))
-        # print('spatial saved_features shape:', spatial_features.shape)
         rgb_hist_features = ch.color_hist_rgb(rgb_img=rgb_image, features_only=True)
-        # print('rgb histogram saved_features shape:', rgb_hist_features.shape)
         rgb_hog_features = he.get_hog_features(multi_channel_img=rgb_image)
-        # print('rgb hog saved_features shape:', rgb_hog_features.shape)
         img_features = np.concatenate((spatial_features, rgb_hist_features, rgb_hog_features))
         features.append(img_features)
     return features
 
 
+def extract_features_from_image(rgb_image, hog_features=None):
+    spatial_features = cs.bin_spatial(img=rgb_image, size=(32, 32))
+    rgb_hist_features = ch.color_hist_rgb(rgb_img=rgb_image, features_only=True)
+    if hog_features is None:
+        rgb_hog_features = he.get_hog_features(multi_channel_img=rgb_image)
+    else:
+        rgb_hog_features = hog_features
+    img_features = np.concatenate((spatial_features, rgb_hist_features, rgb_hog_features))
+    return img_features
