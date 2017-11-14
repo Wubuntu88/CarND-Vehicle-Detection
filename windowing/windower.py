a0f8d1bf43c32cfb835ef5be5a5106992754d06b
William Gillespie <wgilles1@emich.edu>
2017-11-13 19:04:10 -0500

    Code for Windowing.
    creates windows, and makes a prediction on whether that window has a vehicle in it.

---
+++ windowing/windower.py
@@ -0,0 +1,95 @@
+import numpy as np
+import cv2
+
+import feature_extraction.feature_extractor as fe
+
+
+def search_windows(img, windows, classifier, scaler):
+    """
+    Define a function you will pass an image
+    and the list of windows to be searched (output of slide_windows())
+    """
+    # Create an empty list to receive positive detection windows
+    on_windows = []
+    # Iterate over all windows in the list
+    for window in windows:
+        # Extract the test window from original image
+        test_img = cv2.resize(img[window[0][1]:window[1][1], window[0][0]:window[1][0]], (64, 64))
+        # Extract features for that window using single_img_features()
+        features = fe.extract_features_from_image(rgb_image=test_img)
+        # Scale extracted features to be fed to classifier
+        test_features = scaler.transform(np.array(features).reshape(1, -1))
+        # Predict using your classifier
+        prediction = classifier.predict(test_features)
+        # If positive (prediction == 1) then save the window
+        if prediction == 1:
+            on_windows.append(window)
+    # Return windows for positive detections
+    return on_windows
+
+
+def slide_window(img, x_start_stop=[None, None], y_start_stop=[None, None],
+                 xy_window=[64, 64], xy_overlap=[0.5, 0.5]):
+    """
+    Define a function that takes an image,
+    start and stop positions in both x and y,
+    window size (x and y dimensions),
+    and overlap fraction (for both x and y)
+    :param img:
+    :param x_start_stop:
+    :param y_start_stop:
+    :param xy_window:
+    :param xy_overlap:
+    :return:
+    """
+    # If x and/or y start/stop positions not defined, set to image size
+    if x_start_stop[0] is None:
+        x_start_stop[0] = 0
+    if x_start_stop[1] is None:
+        x_start_stop[1] = img.shape[1]
+    if y_start_stop[0] is None:
+        y_start_stop[0] = 0
+    if y_start_stop[1] is None:
+        y_start_stop[1] = img.shape[0]
+
+    # Compute the span of the region to be searched
+    xspan = x_start_stop[1] - x_start_stop[0]
+    yspan = y_start_stop[1] - y_start_stop[0]
+    # Compute the number of pixels per step in x/y
+    nx_pix_per_step = np.int(xy_window[0] * (1 - xy_overlap[0]))
+    ny_pix_per_step = np.int(xy_window[1] * (1 - xy_overlap[1]))
+    # Compute the number of windows in x/y
+    nx_buffer = np.int(xy_window[0] * (xy_overlap[0]))
+    ny_buffer = np.int(xy_window[1] * (xy_overlap[1]))
+    nx_windows = np.int((xspan - nx_buffer) / nx_pix_per_step)
+    ny_windows = np.int((yspan - ny_buffer) / ny_pix_per_step)
+    # Initialize a list to append window positions to
+    window_list = []
+    # Loop through finding x and y window positions
+    # Note: you could vectorize this step, but in practice
+    # you'll be considering windows one by one with your
+    # classifier, so looping makes sense
+    for ys in range(ny_windows):
+        for xs in range(nx_windows):
+            # Calculate window position
+            startx = xs * nx_pix_per_step + x_start_stop[0]
+            endx = startx + xy_window[0]
+            starty = ys * ny_pix_per_step + y_start_stop[0]
+            endy = starty + xy_window[1]
+
+            # Append window position to list
+            window_list.append(((startx, starty), (endx, endy)))
+    # Return the list of windows
+    return window_list
+
+
+# Define a function to draw bounding boxes
+def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
+    # Make a copy of the image
+    imcopy = np.copy(img)
+    # Iterate through the bounding boxes
+    for bbox in bboxes:
+        # Draw a rectangle given bbox coordinates
+        cv2.rectangle(imcopy, bbox[0], bbox[1], color, thick)
+    # Return the image copy with boxes drawn
+    return imcopy
