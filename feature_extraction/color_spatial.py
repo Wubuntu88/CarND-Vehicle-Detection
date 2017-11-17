adb9c15b2d34b606a18690f46af39d13566cbf98
William Gillespie <wgilles1@emich.edu>
2017-11-16 19:40:32 -0500

    Added documentation and refactored the color_spatial method for brevity.

--- feature_extraction/color_spatial.py
+++ feature_extraction/color_spatial.py
@@ -5,7 +5,11 @@ Taken from the Udacity course material
 
 
 def bin_spatial(img, size=(32, 32)):
-    # Use cv2.resize().ravel() to create the feature vector
-    features = cv2.resize(img, size).ravel()
-    # Return the feature vector
-    return features
+    """
+    resize and image to a given size and then turns it into a feature vector.
+    The image is resized so that the feature vectors are of the same length accross different sized images.
+    :param img: An image in any (x, y, depth) dimension.
+    :param size: the dimension to resize the image to.
+    :return: the feature vector of the image unraveled and turned into a 1d array.
+    """
+    return cv2.resize(img, size).ravel()
